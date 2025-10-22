f7a1f91944439fed313fd95f851191c18f82cc6b
Mikosa1237 <janowskimiu1234@gmail.com>
2025-10-22 22:38:03 +0200

    comit pierwszy load działa

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pictureBox = new PictureBox();
+            loadButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            pictureBox.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox.Location = new Point(163, 12);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(600, 400);
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox.TabIndex = 0;
+            pictureBox.TabStop = false;
+            pictureBox.Click += pictureBox1_Click;
+            // 
+            // loadButton
+            // 
+            loadButton.BackColor = Color.Gray;
+            loadButton.Location = new Point(37, 381);
+            loadButton.Name = "loadButton";
+            loadButton.Size = new Size(100, 30);
+            loadButton.TabIndex = 1;
+            loadButton.Text = "Load";
+            loadButton.UseVisualStyleBackColor = false;
+            loadButton.Click += load_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(loadButton);
+            Controls.Add(pictureBox);
+            Name = "Form1";
+            Text = "Image Viewer";
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button loadButton;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,25 @@ namespace obrazek___branche
         {
             InitializeComponent();
         }
+
+        private void load_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Pliki graficzne |*.jpg; *.jpeg; *.png; *.bmp|Wszystkie pliki (*.*)|*.*";
+            ofd.Title = "Wybierz plik graficzny do wczytania";
+            ofd.ShowDialog();
+
+            if (ofd.FileName != "")
+            {
+                Bitmap bitmapa = new Bitmap(ofd.FileName);
+                pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox.Image = bitmapa;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            // Funkcja do zaimplementowania w przyszłości
+        }
     }
 }
